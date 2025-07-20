ca2b597e5041b2cc0e546271cb2ce02018c9481a
Ikmyeongs <boroson@naver.com>
2025-07-20 23:29:08 +0900

    [Bronze V] Title: 꼬마 정민, Time: 116 ms, Memory: 9472 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,11 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('', (input) => {
+  const numbers = input.split(' ').map(Number);
+  const sum = numbers[0] + numbers[1] + numbers[2];
+  console.log(sum);
+  readline.close();
+});
